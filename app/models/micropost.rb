472bfa20e1b4618b490f594fd3e8bc1acf01ef71
hiro.kane <k.hiro1910@gmail.com>
2021-02-08 15:37:34 +0000

    favorite, unfavorite

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,10 @@ class Micropost < ApplicationRecord
   belongs_to :user
 
   validates :content, presence: true, length: { maximum: 255 }
+  
+  has_many :favorites , dependent: :destroy
+  has_many :likes, through: :favorites, source: :micropost
+  has_many :reverses_of_favorites, class_name: 'Favorite', foreign_key: 'micropost_id'
+  has_many :unlikes, through: :reverses_of_favorites, source: :user
+
 end
